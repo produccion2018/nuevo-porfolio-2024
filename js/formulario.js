0c7cf6414930d7d5c2dfe69f97aa6b61ea8d781d
mauro pinto <mauroalexanderpinto@gmail.com>
2024-11-29 11:56:15 -0300

    actualización

--- js/formulario.js
+++ js/formulario.js
@@ -1,6 +1,39 @@
+document.querySelector('.contacto-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // 
+    
+    
+    const nombre = document.querySelector('input[name="nombre"]').value;
+    const email = document.querySelector('input[name="email"]').value;
+    const mensaje = document.querySelector('textarea[name="mensaje"]').value;
+    
+    // Validación de los campos
+    if (nombre === '' || email === '' || mensaje === '') {
+      Swal.fire({
+        icon: 'error',
+        title: '¡Oops!',
+        text: 'Todos los campos son obligatorios.',
+      });
+    } else if (!validateEmail(email)) {
+      Swal.fire({
+        icon: 'error',
+        title: '¡Oops!',
+        text: 'Por favor ingresa un email válido.',
+      });
+    } else {
+      // Si la validación es correcta
+      Swal.fire({
+        icon: 'success',
+        title: 'Formulario enviado',
+        text: 'Tu mensaje ha sido enviado correctamente.',
+      }).then(() => {
+        // Limpiar los campos del formulario
+        document.querySelector('.contacto-form').reset();
+      });
+    }
+  });
 
-/* scripts.js */
-window.addEventListener("load", () => {
-    const loader = document.querySelector(".loader");
-    loader.style.display = "none"; // Ocultar el loader al cargar
-});
+  // Función para validar el formato del email
+  function validateEmail(email) {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return regex.test(email);
+  }
